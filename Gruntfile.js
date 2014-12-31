77e61ed2ba26a88000928aad07e60d8a7e98a6f4
Ignacio Lago <ignacio@glue.gl>
2014-12-31 14:10:51 +0100

    First version. Basic functions. No tests yet.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+/*
+ * grunt-module-index
+ *
+ * Copyright (c) 2014 Ignacio Lago
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      },
+    }
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // By default, lint and build.
+  grunt.registerTask('default', ['jshint']);
+};
